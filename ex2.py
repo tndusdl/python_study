ad302e8b8a0087b64bca3aefcb217c728dad781d
수연 <162692823+tndusdl@users.noreply.github.com>
2024-05-25 23:41:22 +0900

    Add files via upload

---
+++ ex2.py
@@ -0,0 +1,21 @@
+a=5
+if a == 5:
+    print('Right!')
+    print('a is 5')
+    
+if a == 3:
+    print('Right!')
+    print('a is 3')    
+    
+if a != 3:
+    print('Right!')
+    print('a is not 3')
+    
+b=5
+
+if b < 5:
+    print("Small")
+elif b > 5:
+    print("Big")
+else:
+    print("5")
---
+++ ex4.py
@@ -0,0 +1,11 @@
+sum = 0
+i = 0
+
+for i in range(1, 101):
+    sum += i
+    print(sum)
+    
+while i <=99:
+    sum+=i
+    i+=1
+    print(sum)
