db7d1240a5b76cd908503f795b4c6130d55a6da9
수연 <162692823+tndusdl@users.noreply.github.com>
2024-07-08 14:23:07 +0900

    Add files via upload

---
+++ ex1.py
@@ -0,0 +1,20 @@
+def printResult(scores, total):
+    n = len(scores)
+    print(total)
+    print(scores)
+    print(total / n)
+
+def main():
+    scores = []
+    n = int(input("학생 수를 입력하세요: "))
+    
+    total = 0
+    
+    for i in range(0, n):
+        jumsu = int(input("[%d] 영어시험 성적: " % (i+1)))
+        total += jumsu
+        scores.append(jumsu)
+    
+    printResult(scores, total)
+
+main()  
---
+++ ex2.py
@@ -0,0 +1,5 @@
+import md
+("10~20 사이의 소수 :")
+for j in range(10.20) :
+    if md.isPrime(j) :
+        print(j)
